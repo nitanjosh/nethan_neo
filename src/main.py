16ebb0f4cd21349439528a6635079dec45146138
Nethan Josh Bisares <nitanjosh@Nethans-MacBook-Air.local>
2026-02-05 10:44:41 +0800

    please fix

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from ui.page_1 import app as page_1_app
-from data_cleaning import replace_with_none, trim_whitespace, remove_duplicates
 
 st.set_page_config(page_title="Data Processing Application", layout="wide")
 st.sidebar.title("Navigation")
